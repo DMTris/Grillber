bbebc0bf2355aaa29b12f3254aaea09616438ead
DMTris <clangdale5309@gmail.com>
2017-03-25 18:00:17 -0600

    Add grill 1-3 and grillTotal

---
+++ src/data/grilldata.js
@@ -0,0 +1,16 @@
+import Grill from './grills.js';
+/**
+  * constructor method
+  * @param brand
+  * @param series
+  * @param name
+  * @param fuel
+  * @param size
+*/
+let grillOne = new Grill( 'Weber', 'Spirit', 'S-210', 'Gas', 'Medium');
+let grillTwo = new Grill( 'Weber', 'Summit', 'S-470', 'Gas', 'Large');
+let grillThree = new Grill( 'Weber', 'Q', '1200', 'Gas', 'Small');
+
+let grillTotal = [ grillOne, grillTwo, grillThree ];
+
+export defaul grillTotal;
