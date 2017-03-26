c0685a3cf6fc0abc5d53f61203207c2daedb35be
DMTris <clangdale5309@gmail.com>
2017-03-25 19:08:06 -0600

    Add grillFour and grillFive

--- src/data/grilldata.js
+++ src/data/grilldata.js
@@ -7,9 +7,11 @@ import Grill from './grills.js';
   * @param fuel
   * @param size
 */
-let grillOne = new Grill( 'Weber', 'Spirit', 'S-210', 'Gas', 'Medium');
-let grillTwo = new Grill( 'Weber', 'Summit', 'S-470', 'Gas', 'Large');
-let grillThree = new Grill( 'Weber', 'Q', '1200', 'Gas', 'Small');
+let grillOne = new Grill( 'Weber', 'Spirit', 'S-210', 'Gas', 'Medium' );
+let grillTwo = new Grill( 'Weber', 'Summit', 'S-470', 'Gas', 'Large' );
+let grillThree = new Grill( 'Weber', 'Q', '1200', 'Gas', 'Small' );
+let grillFour = new Grill( 'Weber', 'Original Kettle', 'The 22', 'Charcoal', 'Medium' );
+let grillFive = new Grill( 'Weber', 'Ranch Kettle', 'The 44', 'Charcoal', 'Large' );
 
 let grillTotal = [ grillOne, grillTwo, grillThree ];
 
